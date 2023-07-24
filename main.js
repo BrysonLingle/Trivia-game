31d3af76a75cac8d7057b6ef06aa3e2e38cf6157
BrysonLingle <brysonburkett99@gmail.com>
2023-07-24 10:28:53 -0600

    update 3

--- main.js
+++ main.js
@@ -1,10 +1,49 @@
-// var questions = {[
-//     question: 'What was the first car brand?',
+// // button.addEventListener('click', function() {
 
-// ]
-// // }
-// let questions {
-//     title: 'What was the first car brand?',
-//     options: ['Peugeot', 'General Motors', 'Ford', 'Tesla' ]
-//     correctAnswer: 2
+// // })
+
+
+// // function questions (){
+// //     var score = 0
+// //     document.getElementsById('answerButtons1').answered
+// //     {score ++;
+
+// //         }
+// //     }
+// const carQuestions = [{
+//    question: 'What was the first car brand',
+//    answers: {a: 'Peugeot', b: 'General Motors', c: 'Ford', d: 'Tesla'},
+//    correctAnswer: 'a'
 // }
+// ]
+// //  }
+// //  function questionsNum1 (question1){
+// //    let firstQuestion1 = document.getElementById('firstQuestion');
+// //    firstQuestion1.textContent = question1.firstQuestion
+// //    let answers1 = document.querySelectorAll('.Buttons1')
+// //    console.log(answers1)
+// //  answers1.forEach(function(element, index){
+// //    element.textContent =question1.buttons1s[index]
+// //  })
+
+// // }
+
+const carQuestions = [{
+   q: 'What was the first car brand?',
+   a: {
+      answer1: 'Peugeot',
+      answer2: 'General Motors',
+      answer3: 'Ford',
+      answer4: 'Tesla'
+   }
+}]
+const pressButton = document.getElementById('check')
+pressButton.addEventListener('click' , answerS)
+function answerS() {
+   let number = 0
+   for (let i=0; i < carQuestions.length; i++){
+      
+   }
+
+}
+
