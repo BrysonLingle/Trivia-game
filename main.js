64a4cb2960678e7a111156e8b887ed995c4b8c07
BrysonLingle <brysonburkett99@gmail.com>
2023-07-26 10:24:56 -0600

    update 6

--- main.js
+++ main.js
@@ -1,54 +1,77 @@
+let round = 0; //ADDED rounds to track which question you are on
 
-const carQuestions = [{
+const currentScore = document.getElementById('currentScore');
+console.log(currentScore);
+let carQuestions = [
+  {
    question: 'What was the first car brand?',
    correctAnswer: 'Peugeot',
    options: ['Peugeot', "General Motors", 'Ford', 'Tesla']
-}]
+  },
+  {
+   question: 'What is the most popular car in the world? ',
+   correctAnswer: 'Toyota Corrola',
+   options: ['Ford Fiesta', 'Chevy Malibu', 'Toyota Corrola', 'Hyndai Elantra']
+  },
+  {
+   question: 'Who founded the Ford Motor Company?',
+   correctAnswer: 'Henry Ford',
+   options: ['Henry Ford', 'Elon Musk', 'Thomas Edison', 'Alexander Graham Bell']
+  }
+ ];
 
-// let questionsCurrent = carQuestions(question.length - 1)
-let score = 0
-const answered = 0
-const questions1 = document.querySelector('#questionField')
-console.log(questions1)
-const answerButtons = document.querySelectorAll('Button')
+const option1 = document.getElementById('option1');
+
+let score = 0;
+const questions1 = document.querySelector('#questionField');
+console.log(questions1);
+const answerButtons = document.querySelectorAll('.question-buttons'); //ADDED classes to the question buttons and specified you only wanted .question-buttons so you don't add the Next Question button to the array as well
 
 answerButtons.forEach(function (btn) {
-   btn.addEventListener('click', checkAnswer)
-} )
+  btn.addEventListener('click', function(event) {
+   checkAnswer(event);
+  });
+});
 
 function renderQuestion() {
-   questions1.innerText = carQuestions[0].question
-   for (let i = 0; i < answerButtons.length; i++) {
-       console.log(carQuestions[0].options[i])
-       let buttonsNow = answerButtons[i]
-       buttonsNow.querySelector('span').innerText = carQuestions[0].options[i]
-   }
-
+  questions1.innerText = carQuestions[round].question; //REPLACED the 0 with round so as the rounds increase, it moves through the carQuestions array
+  for (let i = 0; i < answerButtons.length; i++) {
+    let buttonsNow = answerButtons[i];
+    buttonsNow.querySelector('span').innerText= carQuestions[round].options[i]; //REPLACED the 0 with round so as the rounds increase, it moves through the carQuestions array
+  }
 }
 
-renderQuestion()
+const nextButton = document.querySelector('#nextButton')
 
-function checkAnswer(c) {
-   let answer = ""
-   if (c.target.tagName === 'BUTTON') {
-       answer = c.target.querySelector('span').innerText
-       
-      } else  { 
-      answer = c.target.innerText
-      console.log(answer)
-     }
-      if (c.target.innerText === carQuestions[0].correctAnswer ) {
-      //  score++;
-      //  answered.push(0);
-      score = i++
-      
-      // console.log('this is true')//next step make correct answer green
-      
-      }
-   function loadscore(){
-      const CurrentScore = document.getElementbyId('currentScore')
-      currentScore.textContent = score
-   }
-   
-   function loadscore() 
+nextButton.addEventListener('click', function(){
+  round++ //ADDED an increase to the round variable to the above renderQuestion function moves on to the next question in carQuestions
+  renderQuestion()
+})
+
+
+renderQuestion();
+
+function checkAnswer(event) {
+  let answer = "";
+  if (event.target.tagName === 'BUTTON') {
+    answer = event.target.querySelector('span').innerText;
+  } else {
+    answer = event.target.innerText;
+    console.log(answer);
+  }
+
+  if (answer === carQuestions[0].correctAnswer) {
+    score++;
+    console.log('Correct Answer!');
+   // event.target.classlist.add('correct-answer')
+    loadscore();
+  }else {
+   console.log('Wrong Answer')
+   // event.target.classlist.add('wrong-answer')
+  }
+
+  function loadscore() {
+   const currentScoreEl = document.getElementById('currentScore');
+   currentScoreEl.textContent = score;
+  }
 }
